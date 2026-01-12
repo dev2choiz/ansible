7a3e03ad579e4da4c3afcb98c882783083f03def
dev2choiz <dev2choiz@gmail.com>
2026-01-12 10:47:04 +0100

    feat(neovim): add eslint

--- files/nvim/config/lua/community.lua
+++ files/nvim/config/lua/community.lua
@@ -14,8 +14,11 @@ return {
 	{ import = "astrocommunity.colorscheme.catppuccin" },
 	{ import = "astrocommunity.pack.go" },
 	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.eslint" },
+	{ import = "astrocommunity.pack.html-css" },
 	{ import = "astrocommunity.pack.docker" },
 	{ import = "astrocommunity.utility.noice-nvim" },
+	{ import = "astrocommunity.git.diffview-nvim" },
 	--- https://github.com/mg979/vim-visual-multi/blob/a6975e7c1ee157615bbc80fc25e4392f71c344d4/doc/visual-multi.txt
 	{ import = "astrocommunity.editing-support.vim-visual-multi" },
 }
