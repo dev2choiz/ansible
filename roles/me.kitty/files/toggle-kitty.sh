e84348c6e8857cb93d2ba55f9ae3d66383365f9f
dev2choiz <dev2choiz@gmail.com>
2026-01-22 16:47:33 +0100

    upgrade golang version

--- roles/me.kitty/files/toggle-kitty.sh
+++ roles/me.kitty/files/toggle-kitty.sh
@@ -4,17 +4,17 @@ TARGET="kitty-dropdown"
 WINID=$(xdotool search --class "$TARGET" | head -n 1)
 
 if [ -z "$WINID" ]; then
-    # kitty window not found => launch it
-    kitty --class "$TARGET" &
+  # kitty window not found => launch it
+  kitty --class "$TARGET" &
 else
-    # toggle minimize / restore
-    STATE=$(xdotool getwindowfocus 2>/dev/null)
-    if [ "$STATE" = "$WINID" ]; then
-        # focused => minimize
-        xdotool windowminimize $WINID
-    else
-        # not focused => restore + focus
-        xdotool windowmap $WINID
-        xdotool windowactivate $WINID
-    fi
+  # toggle minimize / restore
+  STATE=$(xdotool getwindowfocus 2>/dev/null)
+  if [ "$STATE" = "$WINID" ]; then
+    # focused => minimize
+    xdotool windowminimize $WINID
+  else
+    # not focused => restore + focus
+    xdotool windowmap $WINID
+    xdotool windowactivate $WINID
+  fi
 fi
