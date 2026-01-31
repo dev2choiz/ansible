e12d1733dcdd21e203fcdcc37062cd4aa2b499db
dev2choiz <dev2choiz@gmail.com>
2026-01-31 11:13:54 +0100

    feat: install lazydocker

--- scripts/install-nvim-one-liner.sh
+++ scripts/install-nvim-one-liner.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ "$USER" == "root" ]; then
-    echo "This script should not be run as root."
-    exit 1
+  echo "This script should not be run as root."
+  exit 1
 fi
 
 sudo apt install -y git
@@ -13,7 +13,7 @@ TMP_DIR=$(mktemp -d)
 trap "rm -rf $TMP_DIR" EXIT
 
 echo "Cloning repository into $TMP_DIR..."
-git clone https://github.com/dev2choiz/ansible.git "$TMP_DIR"
+git clone --depth 1 https://github.com/dev2choiz/ansible.git "$TMP_DIR"
 
 cd "$TMP_DIR"
 
