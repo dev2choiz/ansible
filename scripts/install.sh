a348fc930c6508deb77723c3073e89b372ee491d
dev2choiz <dev2choiz@gmail.com>
2026-02-04 21:51:15 +0100

    fix: linting

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,13 +4,13 @@ set -e
 STARTTIME=$(date +%s)
 
 if [ "$USER" == "root" ]; then
-    echo "This script should not be run as root."
-    exit 1
+  echo "This script should not be run as root."
+  exit 1
 fi
 
 if [ ! -f ./vars/main.yml ]; then
-    echo "./vars/main.yml file does not exists. Please create it by taking as example the file ./vars/main.yml.example"
-    exit 1
+  echo "./vars/main.yml file does not exists. Please create it by taking as example the file ./vars/main.yml.example"
+  exit 1
 fi
 
 sudo apt install -y make ansible
@@ -18,10 +18,10 @@ ansible-galaxy install -r requirements.yml
 
 TAGS_ARG=""
 for arg in "$@"; do
-    if [[ "$arg" == --tags=* ]]; then
-        TAGS_ARG="--tags ${arg#--tags=}"
-        break
-    fi
+  if [[ "$arg" == --tags=* ]]; then
+    TAGS_ARG="--tags ${arg#--tags=}"
+    break
+  fi
 done
 
 ansible-playbook playbook.yml $TAGS_ARG
