37b3b2c246e4bfcb7a1704100920c9e02c8f314b
dev2choiz <dev2choiz@gmail.com>
2026-02-05 07:15:21 +0100

    feat(nvim): search and replace in the current buffer

--- files/nvim/lazyvim_config/lua/core/keymaps.lua
+++ files/nvim/lazyvim_config/lua/core/keymaps.lua
@@ -188,6 +188,25 @@ M.grug_far = {
     desc = "Search & Replace",
     mode = { "n", "x" },
   },
+  {
+    "<leader>srf",
+    function()
+      local grug = require("grug-far")
+      local is_file = vim.bo.buftype == ""
+      local ext = is_file and vim.fn.expand("%:e")
+      local current_file = is_file and vim.fn.expand("%:p")
+
+      grug.open({
+        transient = true,
+        prefills = {
+          filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          paths = current_file,
+        },
+      })
+    end,
+    desc = "Search & Replace in this file",
+    mode = { "n", "x" },
+  },
 }
 
 return M
