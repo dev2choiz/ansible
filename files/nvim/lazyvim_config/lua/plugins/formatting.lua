9021c0582309e0892840f2d85f75b0bf4001d6cd
dev2choiz <dev2choiz@gmail.com>
2026-03-07 15:19:04 +0100

    feat: add rust role into playbook and install in docker

--- files/nvim/lazyvim_config/lua/plugins/formatting.lua
+++ files/nvim/lazyvim_config/lua/plugins/formatting.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        go = { "gofumpt", "goimports" },
+        go = { "golangci-lint" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
