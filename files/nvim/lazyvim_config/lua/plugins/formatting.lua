e8e98233b87cd6307902fc7795a039cd005591a8
dev2choiz <dev2choiz@gmail.com>
2026-03-16 09:57:53 +0100

    feat: select dynamically js formatter

--- files/nvim/lazyvim_config/lua/plugins/formatting.lua
+++ files/nvim/lazyvim_config/lua/plugins/formatting.lua
@@ -1,16 +1,27 @@
+local function js_formatter()
+  local root = require("core.utils.fs").get_root()
+
+  if vim.fn.filereadable(root .. "/biome.json") == 1 then
+    return { "biome-check" } -- runs formatting, linting and import sorting
+  end
+
+  return { "prettier" }
+end
+
 return {
   {
     "stevearc/conform.nvim",
     opts = {
+      default_format_opts = { lsp_format = "fallback" },
       formatters_by_ft = {
         lua = { "stylua" },
         go = { "golangci-lint" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        vue = { "prettier" },
-        json = { "prettier" },
+        javascript = js_formatter,
+        typescript = js_formatter,
+        javascriptreact = js_formatter,
+        typescriptreact = js_formatter,
+        vue = js_formatter,
+        json = js_formatter,
         python = { "ruff_format" },
         sh = { "shfmt" },
       },
--- files/nvim/lazyvim_config/lua/plugins/me_mason.lua
+++ files/nvim/lazyvim_config/lua/plugins/me_mason.lua
@@ -1,17 +1,12 @@
 return {
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
-        -- golang
-        "gopls",
-        "gotests",
-        "goimports",
-        -- yaml
-        "yaml-language-server",
-        "vue-language-server",
-      })
-    end,
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "biome",
+        -- "vue-language-server",
+      },
+    },
   },
 }
