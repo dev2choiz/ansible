3be95ea8fae4f955a0002b2c2b4bb0c8c31f199d
dev2choiz <dev2choiz@gmail.com>
2026-05-20 14:14:59 +0200

    fix(nvim): fix lazy loading for comment.nvim

--- files/nvim/lazyvim_config/lua/plugins/comment.lua
+++ files/nvim/lazyvim_config/lua/plugins/comment.lua
@@ -1,37 +1,52 @@
 return {
   {
     "numToStr/Comment.nvim",
+    lazy = true,
     opts = {
       mappings = {
+        -- With `mappings.basic = false` and `mappings.extra = false` the plugin won't create any mappings
+        -- then rewrite the mapping manually, including only the block part (`gb`, `gbc`)
+        -- For line part (`gc`, `gcc`), we will use the neovim / ts-comment.nvim functionality
         basic = false,
         extra = false,
       },
     },
-    config = function(_, opts)
-      require("Comment").setup(opts)
-
-      -- With `mappings.basic = false` and `mappings.extra = false` the plugin won't create any mappings
-      -- then rewrite the mapping manually, including only the block part (`gb`, `gbc`)
-      -- For line part (`gc`, `gcc`), we will use the neovim native functionality
-      --
-      -- Copied from the source: https://github.com/numToStr/Comment.nvim/blob/master/lua/Comment/init.lua#L98
-      local api = require("Comment.api")
-      local vvar = vim.api.nvim_get_vvar
-      local K = vim.keymap.set
-
+    keys = {
+      -- Inspired by the source: https://github.com/numToStr/Comment.nvim/blob/master/lua/Comment/init.lua#L98
       -- NORMAL mode mappings
-      K("n", "gb", "<Plug>(comment_toggle_blockwise)", { desc = "Comment toggle blockwise" })
-
-      K("n", "gbc", function()
-        return vvar("count") == 0 and "<Plug>(comment_toggle_blockwise_current)"
-          or "<Plug>(comment_toggle_blockwise_count)"
-      end, { expr = true, desc = "Comment toggle current block" })
-
+      {
+        "gb",
+        "<Plug>(comment_toggle_blockwise)",
+        mode = "n",
+        desc = "Comment toggle blockwise",
+      },
+      {
+        "gbc",
+        function()
+          return vim.api.nvim_get_vvar("count") == 0 and "<Plug>(comment_toggle_blockwise_current)"
+            or "<Plug>(comment_toggle_blockwise_count)"
+        end,
+        mode = "n",
+        expr = true,
+        desc = "Comment toggle current block",
+      },
       -- VISUAL mode mappings
-      K("x", "gb", "<Plug>(comment_toggle_blockwise_visual)", { desc = "Comment toggle blockwise (visual)" })
-
+      {
+        "gb",
+        "<Plug>(comment_toggle_blockwise_visual)",
+        mode = "x",
+        desc = "Comment toggle blockwise (visual)",
+      },
       -- Extra Mappings
-      K("n", "gcA", api.locked("insert.linewise.eol"), { desc = "Comment insert end of line" })
-    end,
+      {
+        "gcA",
+        function()
+          -- require("Comment.api").locked("insert.linewise.eol")
+          require("Comment.api").insert.linewise.eol()
+        end,
+        mode = "n",
+        desc = "Comment insert end of line",
+      },
+    },
   },
 }
