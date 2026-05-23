88fb9fe172e372eec437cfbb732f87629a4d1103
dev2choiz <dev2choiz@gmail.com>
2026-05-23 18:39:19 +0200

    fix(nvim): prevent mason crash on first startup for vue-language-server

--- files/nvim/lazyvim_config/lua/plugins/me_lsp.lua
+++ files/nvim/lazyvim_config/lua/plugins/me_lsp.lua
@@ -1,3 +1,17 @@
+local has_mason, registry = pcall(require, "mason-registry")
+local is_vue_installed = has_mason and registry.is_installed("vue-language-server")
+local old_get_pkg_path = LazyVim.get_pkg_path
+
+if not is_vue_installed and old_get_pkg_path then
+  LazyVim.get_pkg_path = function(pkg, path, opts)
+    if pkg == "vue-language-server" then
+      print("vue-language-server not found")
+      return ""
+    end
+    return old_get_pkg_path(pkg, path, opts)
+  end
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
