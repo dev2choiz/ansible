7c40d0660ce4cdcd29c399db1df27617389cdad2
dev2choiz <dev2choiz@gmail.com>
2026-06-09 12:58:34 +0200

    feat(nvim): disable relative number

--- files/nvim/lazyvim_config/lua/config/options.lua
+++ files/nvim/lazyvim_config/lua/config/options.lua
@@ -1,2 +1,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.o.sessionoptions = "buffers,curdir,tabpages,winsize,winpos,help,globals,skiprtp,folds,localoptions,blank,terminal"
+vim.o.relativenumber = false
