cd5aa7c0859f2abf8d00264de6fcabef9d8fd3e2
dev2choiz <dev2choiz@gmail.com>
2026-07-04 18:45:21 +0200

    feat(dotfiles): alias to switch theme easily

---
+++ files/zsh/zshrc.d/50-aliases.sh
@@ -0,0 +1,24 @@
+theme() {
+  local target="$ZSHRC_D_DIR/misc/theme/$1.sh"
+  local link="$ZSHRC_D_DIR/early.d/05-current-theme.sh"
+
+  if [[ ! -f "$target" ]]; then
+    echo "Theme not found: $target"
+    return 1
+  fi
+
+  ln -sf "$target" "$link"
+  echo "Theme enabled: $1"
+}
+
+_theme() {
+  local files themes
+
+  files=("$ZSHRC_D_DIR/misc/theme/"*.sh)
+
+  themes=(${files:t})
+  themes=(${themes%.sh})
+  compadd "${themes[@]}"
+}
+
+compdef _theme theme
