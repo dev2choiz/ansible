90f8226102fc8114732edbb8aa78252907d5249f
dev2choiz <dev2choiz@gmail.com>
2026-07-24 06:32:42 +0200

    feat(nvim): load constants.lua in the project to override

--- files/nvim/lazyvim_config/lua/core/constants.lua
+++ files/nvim/lazyvim_config/lua/core/constants.lua
@@ -1,3 +1,33 @@
-return {
+local fs = require("core.utils.fs")
+local logger = require("core.utils.logger").with_source("constants")
+
+local M = {
   maxFileSize = 10 * 1024 * 1024,
 }
+
+local function loadConstants()
+  local paths = {}
+
+  -- Global config
+  local global = fs.get_global_config_dir()
+  if global then
+    table.insert(paths, global .. "/constants.lua")
+  end
+
+  -- Project config
+  table.insert(paths, fs.get_root() .. "/.nvim/constants.lua")
+
+  for _, file in ipairs(paths) do
+    if fs.file_exists(file) then
+      local ok, conf = fs.safe_dotfile(file, true)
+      if ok and type(conf) == "table" then
+        M = vim.tbl_deep_extend("force", M, conf)
+        logger.debug("config loaded: " .. file)
+      end
+    end
+  end
+end
+
+loadConstants()
+
+return M
